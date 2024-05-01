862670694d77782bbc65273f9bbf1bb800e39bad
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-05-01 12:12:25 +0530

    leetcode

--- src/socials.js
+++ src/socials.js
@@ -1,30 +1,36 @@
 const socials = [
   {
+    name: "Gmail",
     color: "#FF204E",
     logo: "mdi:gmail",
     link: "https://mail.google.com/mail/?view=cm&fs=1&to=sami.thakur2804@gmail.com",
   },
   {
+    name: "GitHub",
     color: "#000000",
     logo: "mdi:github",
     link: "https://github.com/SAMI-THAKUR",
   },
   {
+    name: "LeetCode",
     color: "#F57D1F",
     logo: "simple-icons:leetcode",
-    link: "https://leetcode.com/user0351hg/",
+    link: "https://leetcode.com/u/SamiThakur/",
   },
   {
+    name: "LinkedIn",
     color: "#0A66C2",
     logo: "mdi:linkedin",
     link: "https://www.linkedin.com/in/sami-thakur-6072b8256/",
   },
   {
+    name: "Twitter",
     color: "#1DA1F2",
     logo: "mdi:twitter",
     link: "https://twitter.com/SamiThakur28",
   },
   {
+    name: "Instagram",
     color: "#F11A7B",
     logo: "mdi:instagram",
     link: "https://www.instagram.com/sami_thakur_/?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
